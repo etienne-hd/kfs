9d8e5fff286a347c91cfcc3fadcd87f3f7aa40f3
nda-cunh <hydraldev@gmail.com>
2026-04-04 23:59:49 +0200

    refactor(build): migrate bootloader to GAS and integrate meson build system

---
+++ boot.s
@@ -0,0 +1,54 @@
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov $stack_top, %esp
+	call main
+
+	cli
+1:	hlt
+	jmp 1b
+
+.size _start, . - _start
+
+
+/*
+; bits 32
+; 
+; section .multiboot				;according to multiboot spec
+		; dd 0x1BADB002			;set magic number for bootloader
+        ; dd 0x0					;set flags
+        ; 
+; 
+; section .text
+; global start
+; extern main						;defined in the C file
+; 
+; start:
+        ; cli						;block interrupts
+        ; mov esp, stack_space	;set stack pointer
+        ; call main
+        ; hlt						;halt the CPU
+; 
+; section .bss
+; resb 16000						;16KB for stack
+; stack_space:
+*/
