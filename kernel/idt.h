e7c2fec6cc0071fa283c67e4bd3917e0775f10a3
nda-cunh <hydraldev@gmail.com>
2026-04-11 01:04:54 +0200

    style(idt): add packed attributes to idt entry and pointer structs

--- kernel/idt.h
+++ kernel/idt.h
@@ -1,19 +1,19 @@
 #ifndef IDT_H
 # define IDT_H
 
-typedef struct s_idt_entry
+typedef struct __attribute__((packed)) s_idt_entry
 {
     unsigned short int offset_lowerbits;
     unsigned short int selector;
     unsigned char zero;
     unsigned char flags;
     unsigned short int offset_higherbits;
-} idt_entry __attribute__((packed));
+} idt_entry;
 
-typedef struct s_idt_pointer
+typedef struct __attribute__((packed)) s_idt_pointer
 {
     unsigned short limit;
     unsigned int base;
-} idt_pointer __attribute__((packed));
+} idt_pointer;
 
 #endif
